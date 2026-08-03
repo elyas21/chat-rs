e98adb170218ddc691b1424db38c23a802348367
elyas21 <elyasabate21@gmail.com>
2026-08-03 19:31:38 +0300

    fix: vite cov env

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/v1': {
-        target: 'http://127.0.0.1:3000',
+        target: process.env.VITE_API_BASE || 'http://127.0.0.1:3000',
         changeOrigin: true,
         secure: false,
       },
